9eb0f412fc31004351dcb57f2d007d983eee8d7a
cercinaai <services@cercina.ai>
2025-03-27 05:17:26 +0100

    Correction du fichier CSS et de la configuration Angular

--- realstate-frontend/src/environments/environment.ts
+++ realstate-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://141.94.240.141:5009/api/v1',
+    apiUrl: 'http://141.94.240.141/api/v1',
     loginUrl: 'http://141.94.240.141/xtracto/signin',
     agencesUrl: 'http://141.94.240.141/xtracto/agences'
   };
